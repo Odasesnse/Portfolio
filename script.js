2182d388f0a907f3f495a46b4eb8d0b35af06576
Romeo <romeoltonsing7@gmail.com>
2025-07-16 19:14:45 +0530

    made changes to all

---
+++ script.js
@@ -0,0 +1,9 @@
+function project() {
+    window.open("https://github.com/Odasesnse", "_blank");
+}
+function projecttwo() {
+    window.open("https://codepen.io/Odasesnse", "_blank");
+}
+function projectall() {
+    window.open("https://github.com/Odasesnse", "_blank");
+}
